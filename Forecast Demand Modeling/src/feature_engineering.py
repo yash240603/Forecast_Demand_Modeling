bdb10ff6eeb2d37c9188c78f500d6b8a624cfb8d
Yash Shirke <yash240603@gmail.com>
2024-12-27 20:48:46 +0530

    Delete Forecast Demand Modeling directory

--- Forecast Demand Modeling/src/feature_engineering.py
+++
@@ -1,9 +0,0 @@
-def generate_features(data):
-    """
-    Generate features from the cleaned dataset.
-    :param data: Cleaned DataFrame.
-    :return: DataFrame with new features.
-    """
-    data['Month'] = data['Date'].dt.month
-    data['Year'] = data['Date'].dt.year
-    return data.drop(['Date'], axis=1)
